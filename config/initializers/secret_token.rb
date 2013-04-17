9538667cf55de931a6dc6f34e4ee72e4f49e30bb
EC2 Default User <ec2-user@ip-10-134-127-147.ap-southeast-1.compute.internal>
2013-04-17 06:20:05 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limsunyoung::Application.config.secret_token = 'd3814755a92b4d99a579542ca82e1008d3302085edc6b9bf6e55d1d36e67975ca71c527c1f6aa30c1d6d3ae4587e1f62d14300ab5b434555f810b6168a45b566'
